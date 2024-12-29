f4276372638b1008a0dac5774aeb8a14f9fbb1f0
neonite2217 <opneonite@gmail.com>
2024-12-29 23:41:59 +0530

    support for scanning the division operator & comments

--- app/main.py
+++ app/main.py
@@ -1,163 +1,363 @@
 import sys
 
-from . import error
+import typing
 
-from .scanner import Scanner
+from .grammar import Token, TokenType
 
-from .parser import Parser
+class Scanner:
 
-from .ast_printer import AstPrinter
+    keywords = {
 
-from .interpreter import Interpreter
+        "and": TokenType.AND,
 
-interpreter = Interpreter()
+        "class": TokenType.CLASS,
 
-def main() -> None:
+        "else": TokenType.ELSE,
 
-    if len(sys.argv) == 1:  # run interactive interpreter
+        "false": TokenType.FALSE,
 
-        run_prompt()
+        "for": TokenType.FOR,
 
-    elif len(sys.argv) > 3:
+        "fun": TokenType.FUN,
 
-        print("Too many args", sys.stderr)
+        "if": TokenType.IF,
 
-    else:  # run file
+        "nil": TokenType.NIL,
 
-        if len(sys.argv) == 3:  # command provided
+        "or": TokenType.OR,
 
-            command = sys.argv[1]
+        "print": TokenType.PRINT,
 
-            filename = sys.argv[2]
+        "return": TokenType.RETURN,
 
-        else:
+        "super": TokenType.SUPER,
 
-            filename = sys.argv[1]
+        "this": TokenType.THIS,
 
-            command = ""
+        "true": TokenType.TRUE,
 
-        if command not in ["", "tokenize", "parse", "evaluate", "run"]:
+        "var": TokenType.VAR,
 
-            print(f"Unknown command: {command}", file=sys.stderr)
+        "while": TokenType.WHILE,
 
-            exit(64)
+    }
 
-        run_file(filename, command)
+    def __init__(self, source: str):
 
-def run_file(filename: str, command: str = "") -> None:
+        self.source = source
+
+        self.tokens: typing.List[Token] = []
+
+        self.start = 0
+
+        self.current = 0
+
+        self.line = 1
+
+        self.had_error = False
+
+    @property
+
+    def is_at_end(self):
+
+        return self.current >= len(self.source)
+
+    @property
+
+    def text(self):
+
+        return self.source[self.start : self.current]
+
+    def scan_tokens(self):
+
+        while not self.is_at_end:
+
+            self.start = self.current
+
+            self.scan_token()
+
+        self.tokens.append(Token(TokenType.EOF, "", None, self.line))
+
+        return self.tokens
+
+    def scan_token(self):
+
+        character = self.advance()
+
+        match character:
+
+            case "(":
+
+                self.add_token(TokenType.LEFT_PAREN)
+
+            case ")":
+
+                self.add_token(TokenType.RIGHT_PAREN)
+
+            case "{":
+
+                self.add_token(TokenType.LEFT_BRACE)
+
+            case "}":
+
+                self.add_token(TokenType.RIGHT_BRACE)
+
+            case ",":
+
+                self.add_token(TokenType.COMMA)
+
+            case ".":
+
+                self.add_token(TokenType.DOT)
+
+            case "-":
+
+                self.add_token(TokenType.MINUS)
+
+            case "+":
+
+                self.add_token(TokenType.PLUS)
+
+            case ";":
+
+                self.add_token(TokenType.SEMICOLON)
+
+            case "*":
+
+                self.add_token(TokenType.STAR)
+
+            case "!":
+
+                self.add_token(
+
+                    TokenType.BANG_EQUAL if self.match("=") else TokenType.BANG
+
+                )
+
+            case "=":
+
+                self.add_token(
+
+                    TokenType.EQUAL_EQUAL if self.match("=") else TokenType.EQUAL
+
+                )
+
+            case "<":
+
+                self.add_token(
+
+                    TokenType.LESS_EQUAL if self.match("=") else TokenType.LESS
+
+                )
+
+            case ">":
+
+                self.add_token(
+
+                    TokenType.GREATER_EQUAL if self.match("=") else TokenType.GREATER
+
+                )
+
+            case "/":
+
+                self.advance_next_line() if self.match("/") else self.add_token(
+
+                    TokenType.SLASH
+
+                )
+
+            case " ":
+
+                pass
+
+            case "\r":
+
+                pass
+
+            case "\t":
+
+                pass
+
+            case "\n":
+
+                self.line += 1
+
+            case '"':
+
+                self.string()
+
+            case _:
+
+                if self.is_number(character):
+
+                    self.number()
+
+                elif self.is_alpha(character):
+
+                    self.identifier()
+
+                else:
+
+                    self.error(self.line, f"Unexpected character: {character}")
+
+    def peek(self, n=0):
+
+        index = self.current + n
+
+        if index >= len(self.source):
+
+            return "\0"
+
+        return self.source[index]
+
+    def advance(self):
+
+        index = self.current
+
+        self.current += 1
+
+        return self.source[index]
+
+    def match(self, expected: str):
+
+        if self.is_at_end:
+
+            return False
+
+        if self.source[self.current] != expected:
+
+            return False
+
+        self.current += 1
+
+        return True
+
+    def advance_next_line(self):
+
+        while self.peek() != "\n" and not self.is_at_end:
+
+            self.advance()
+
+    def add_token(self, type: TokenType, literal: typing.Any = None):
+
+        self.tokens.append(Token(type, self.text, literal, self.line))
+
+    def error(self, line: int, message: str):
+
+        self.report(line, "", message)
+
+    def report(self, line: int, where: str, message: str):
+
+        print(f"[line {line}] Error{where}: {message}", file=sys.stderr)
+
+        self.had_error = True
+
+    def string(self):
+
+        while self.peek() != '"' and not self.is_at_end:
+
+            if self.peek() == "\n":
+
+                self.line += 1
+
+            self.advance()
+
+        if self.is_at_end:
+
+            self.error(self.line, "Unterminated string.")
+
+            return
+
+        # closing "
+
+        self.advance()
+
+        value = self.source[self.start + 1 : self.current - 1]
+
+        self.add_token(TokenType.STRING, value)
+
+    def number(self):
+
+        while self.is_number(self.peek()):
+
+            self.advance()
+
+        if self.peek() == "." and self.is_number(self.peek(1)):
+
+            # consume .
+
+            self.advance()
+
+            while self.is_number(self.peek()):
+
+                self.advance()
+
+        value = float(self.text)
+
+        self.add_token(TokenType.NUMBER, value)
+
+    def identifier(self):
+
+        while self.is_alpha_or_number(self.peek()):
+
+            self.advance()
+
+        type = self.keywords.get(self.text, TokenType.IDENTIFIER)
+
+        self.add_token(type)
+
+    def is_number(self, character: str):
+
+        return character.isnumeric()
+
+    def is_alpha(self, character: str):
+
+        return character.isalpha() or character == "_"
+
+    def is_alpha_or_number(self, character: str):
+
+        return self.is_number(character) or self.is_alpha(character)
+
+def main():
+
+    if len(sys.argv) < 3:
+
+        print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
+
+        exit(1)
+
+    command = sys.argv[1]
+
+    filename = sys.argv[2]
+
+    if command != "tokenize":
+
+        print(f"Unknown command: {command}", file=sys.stderr)
+
+        exit(1)
 
     with open(filename) as file:
 
         file_contents = file.read()
 
-    match command:
+    scanner = Scanner(file_contents)
 
-        case "run":
-
-            run(file_contents)
-
-        case "evaluate":
-
-            run_eval(file_contents)
-
-        case "parse":
-
-            run_parse(file_contents)
-
-        case "tokenize":
-
-            run_tokenize(file_contents)
-
-            # print(f"[SCANNER] done. Error: {error.hadError}", file=sys.stderr)
-
-        case _:
-
-            run(file_contents)
-
-    if error.hadError:
-
-        exit(65)
-
-    if error.hadRuntimeError:
-
-        exit(70)
-
-def run_prompt() -> None:
-
-    while True:
-
-        try:
-
-            prompt = input("> ")
-
-            run(prompt)
-
-            error.hadError = False
-
-        except EOFError:
-
-            break
-
-def run_tokenize(source: str) -> None:
-
-    scanner = Scanner(source)
-
-    tokens = scanner.scanTokens()
+    tokens = scanner.scan_tokens()
 
     for token in tokens:
 
-        print(token)
+        literal = token.literal
 
-def run_parse(source: str) -> None:
+        if literal is None:
 
-    scanner = Scanner(source)
+            literal = "null"
 
-    tokens = scanner.scanTokens()
+        print(f"{token.type.name} {token.lexeme} {literal}")
 
-    parser = Parser(tokens)
+    if scanner.had_error:
 
-    expression = parser.parse_expr()
+        exit(65)
 
-    if error.hadError or expression is None:
+if __name__ == "__main__":
 
-        return
-
-    print(AstPrinter({}).print(expression))
-
-def run_eval(source: str) -> None:
-
-    scanner = Scanner(source)
-
-    tokens = scanner.scanTokens()
-
-    parser = Parser(tokens)
-
-    expression = parser.parse_expr()
-
-    if error.hadError or expression is None:
-
-        return
-
-    interpreter.interpret_expr(expression)
-
-def run(source: str) -> None:
-
-    scanner = Scanner(source)
-
-    tokens = scanner.scanTokens()
-
-    parser = Parser(tokens)
-
-    statements = parser.parse()
-
-    # for i, s in enumerate(statements):
-
-    #     print(f"[Stmt {i}] {printer.print(s)}")
-
-    if error.hadError:
-
-        return
-
-    interpreter.interpret(statements)
-
-main()
+    main()
