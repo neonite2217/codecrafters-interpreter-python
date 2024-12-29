3cb6f5acd7142da8f2ab5029b46094d582d2e4a1
neonite2217 <opneonite@gmail.com>
2024-12-29 23:10:19 +0530

    Sanning braces

--- app/main.py
+++ app/main.py
@@ -3,41 +3,33 @@ import sys
 def main():
 
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-
     print("Logs from your program will appear here!", file=sys.stderr)
 
     if len(sys.argv) < 3:
-
         print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
-
         exit(1)
 
     command = sys.argv[1]
-
     filename = sys.argv[2]
 
     if command != "tokenize":
-
         print(f"Unknown command: {command}", file=sys.stderr)
-
         exit(1)
 
     with open(filename) as file:
-
         file_contents = file.read()
 
     for c in file_contents:
-
         if c == "(":
-
             print("LEFT_PAREN ( null")
-
         if c == ")":
-
             print("RIGHT_PAREN ) null")
+        if c == "{":
+            print("LEFT_BRACE { null")
+        if c == "}":
+            print("RIGHT_BRACE } null")
 
-    print("EOF  null")
+    print("EOF null")
 
 if __name__ == "__main__":
-
     main()
