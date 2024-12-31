c289638a2e2731671e86ab9a4731021929c959e3
neonite2217 <opneonite@gmail.com>
2024-12-31 16:40:48 +0530

    changes made

--- app/main.py
+++ app/main.py
@@ -1,324 +1,10 @@
 import sys
 
-import typing
-
-from .grammar import Token, TokenType
-
-class Scanner:
-
-    keywords = {
-
-        "and": TokenType.AND,
-
-        "class": TokenType.CLASS,
-
-        "else": TokenType.ELSE,
-
-        "false": TokenType.FALSE,
-
-        "for": TokenType.FOR,
-
-        "fun": TokenType.FUN,
-
-        "if": TokenType.IF,
-
-        "nil": TokenType.NIL,
-
-        "or": TokenType.OR,
-
-        "print": TokenType.PRINT,
-
-        "return": TokenType.RETURN,
-
-        "super": TokenType.SUPER,
-
-        "this": TokenType.THIS,
-
-        "true": TokenType.TRUE,
-
-        "var": TokenType.VAR,
-
-        "while": TokenType.WHILE,
-
-    }
-
-    def __init__(self, source: str):
-
-        self.source = source
-
-        self.tokens: typing.List[Token] = []
-
-        self.start = 0
-
-        self.current = 0
-
-        self.line = 1
-
-        self.had_error = False
-
-    @property
-
-    def is_at_end(self):
-
-        return self.current >= len(self.source)
-
-    @property
-
-    def text(self):
-
-        return self.source[self.start : self.current]
-
-    def scan_tokens(self):
-
-        while not self.is_at_end:
-
-            self.start = self.current
-
-            self.scan_token()
-
-        self.tokens.append(Token(TokenType.EOF, "", None, self.line))
-
-        return self.tokens
-
-    def scan_token(self):
-
-        character = self.advance()
-
-        match character:
-
-            case "(":
-
-                self.add_token(TokenType.LEFT_PAREN)
-
-            case ")":
-
-                self.add_token(TokenType.RIGHT_PAREN)
-
-            case "{":
-
-                self.add_token(TokenType.LEFT_BRACE)
-
-            case "}":
-
-                self.add_token(TokenType.RIGHT_BRACE)
-
-            case ",":
-
-                self.add_token(TokenType.COMMA)
-
-            case ".":
-
-                self.add_token(TokenType.DOT)
-
-            case "-":
-
-                self.add_token(TokenType.MINUS)
-
-            case "+":
-
-                self.add_token(TokenType.PLUS)
-
-            case ";":
-
-                self.add_token(TokenType.SEMICOLON)
-
-            case "*":
-
-                self.add_token(TokenType.STAR)
-
-            case "!":
-
-                self.add_token(
-
-                    TokenType.BANG_EQUAL if self.match("=") else TokenType.BANG
-
-                )
-
-            case "=":
-
-                self.add_token(
-
-                    TokenType.EQUAL_EQUAL if self.match("=") else TokenType.EQUAL
-
-                )
-
-            case "<":
-
-                self.add_token(
-
-                    TokenType.LESS_EQUAL if self.match("=") else TokenType.LESS
-
-                )
-
-            case ">":
-
-                self.add_token(
-
-                    TokenType.GREATER_EQUAL if self.match("=") else TokenType.GREATER
-
-                )
-
-            case "/":
-
-                self.advance_next_line() if self.match("/") else self.add_token(
-
-                    TokenType.SLASH
-
-                )
-
-            case " ":
-
-                pass
-
-            case "\r":
-
-                pass
-
-            case "\t":
-
-                pass
-
-            case "\n":
-
-                self.line += 1
-
-            case '"':
-
-                self.string()
-
-            case _:
-
-                if self.is_number(character):
-
-                    self.number()
-
-                elif self.is_alpha(character):
-
-                    self.identifier()
-
-                else:
-
-                    self.error(self.line, f"Unexpected character: {character}")
-
-    def peek(self, n=0):
-
-        index = self.current + n
-
-        if index >= len(self.source):
-
-            return "\0"
-
-        return self.source[index]
-
-    def advance(self):
-
-        index = self.current
-
-        self.current += 1
-
-        return self.source[index]
-
-    def match(self, expected: str):
-
-        if self.is_at_end:
-
-            return False
-
-        if self.source[self.current] != expected:
-
-            return False
-
-        self.current += 1
-
-        return True
-
-    def advance_next_line(self):
-
-        while self.peek() != "\n" and not self.is_at_end:
-
-            self.advance()
-
-    def add_token(self, type: TokenType, literal: typing.Any = None):
-
-        self.tokens.append(Token(type, self.text, literal, self.line))
-
-    def error(self, line: int, message: str):
-
-        self.report(line, "", message)
-
-    def report(self, line: int, where: str, message: str):
-
-        print(f"[line {line}] Error{where}: {message}", file=sys.stderr)
-
-        self.had_error = True
-
-    def string(self):
-
-        while self.peek() != '"' and not self.is_at_end:
-
-            if self.peek() == "\n":
-
-                self.line += 1
-
-            self.advance()
-
-        if self.is_at_end:
-
-            self.error(self.line, "Unterminated string.")
-
-            return
-
-        # closing "
-
-        self.advance()
-
-        value = self.source[self.start + 1 : self.current - 1]
-
-        self.add_token(TokenType.STRING, value)
-
-    def number(self):
-
-        while self.is_number(self.peek()):
-
-            self.advance()
-
-        if self.peek() == "." and self.is_number(self.peek(1)):
-
-            # consume .
-
-            self.advance()
-
-            while self.is_number(self.peek()):
-
-                self.advance()
-
-        value = float(self.text)
-
-        self.add_token(TokenType.NUMBER, value)
-
-    def identifier(self):
-
-        while self.is_alpha_or_number(self.peek()):
-
-            self.advance()
-
-        type = self.keywords.get(self.text, TokenType.IDENTIFIER)
-
-        self.add_token(type)
-
-    def is_number(self, character: str):
-
-        return character.isnumeric()
-
-    def is_alpha(self, character: str):
-
-        return character.isalpha() or character == "_"
-
-    def is_alpha_or_number(self, character: str):
-
-        return self.is_number(character) or self.is_alpha(character)
-
 def main():
+
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+
+    # print("Logs from your program will appear here!", file=sys.stderr)
 
     if len(sys.argv) < 3:
 
@@ -340,23 +26,293 @@ def main():
 
         file_contents = file.read()
 
-    scanner = Scanner(file_contents)
+    # Uncomment this block to pass the first stage
 
-    tokens = scanner.scan_tokens()
+    line = 1
 
-    for token in tokens:
+    error = False
 
-        literal = token.literal
+    length = len(file_contents)
 
-        if literal is None:
+    i = 0
 
-            literal = "null"
+    reserved_words = [
 
-        print(f"{token.type.name} {token.lexeme} {literal}")
+        "and",
 
-    if scanner.had_error:
+        "class",
+
+        "else",
+
+        "false",
+
+        "for",
+
+        "fun",
+
+        "if",
+
+        "nil",
+
+        "or",
+
+        "print",
+
+        "return",
+
+        "super",
+
+        "this",
+
+        "true",
+
+        "var",
+
+        "while",
+
+    ]
+
+    if file_contents:
+
+        while i < length:
+
+            c = file_contents[i]
+
+            if c == "\n":
+
+                line += 1
+
+            elif c == " " or c == "\r" or c == "\t":
+
+                pass
+
+            elif c == "(":
+
+                print("LEFT_PAREN ( null")
+
+            elif c == ")":
+
+                print("RIGHT_PAREN ) null")
+
+            elif c == "{":
+
+                print("LEFT_BRACE { null")
+
+            elif c == "}":
+
+                print("RIGHT_BRACE } null")
+
+            elif c == ",":
+
+                print("COMMA , null")
+
+            elif c == ";":
+
+                print("SEMICOLON ; null")
+
+            elif c == ".":
+
+                print("DOT . null")
+
+            elif c == "-":
+
+                print("MINUS - null")
+
+            elif c == "+":
+
+                print("PLUS + null")
+
+            elif c == "*":
+
+                print("STAR * null")
+
+            elif c == "=":
+
+                if i + 1 < length and file_contents[i + 1] == "=":
+
+                    i += 1
+
+                    print("EQUAL_EQUAL == null")
+
+                else:
+
+                    print("EQUAL = null")
+
+            elif c == "!":
+
+                if i + 1 < length and file_contents[i + 1] == "=":
+
+                    i += 1
+
+                    print("BANG_EQUAL != null")
+
+                else:
+
+                    print("BANG ! null")
+
+            elif c == "<":
+
+                if i + 1 < length and file_contents[i + 1] == "=":
+
+                    i += 1
+
+                    print("LESS_EQUAL <= null")
+
+                else:
+
+                    print("LESS < null")
+
+            elif c == ">":
+
+                if i + 1 < length and file_contents[i + 1] == "=":
+
+                    i += 1
+
+                    print("GREATER_EQUAL >= null")
+
+                else:
+
+                    print("GREATER > null")
+
+            elif c == "/":
+
+                if i + 1 < length and file_contents[i + 1] == "/":
+
+                    while i < length and file_contents[i] != "\n":
+
+                        i += 1
+
+                    line += 1  # we got to /n
+
+                else:
+
+                    print("SLASH / null")
+
+            elif c == '"':
+
+                word = ""
+
+                i += 1
+
+                while i < length and file_contents[i] != '"':
+
+                    word += file_contents[i]
+
+                    i += 1
+
+                if i == length:
+
+                    error = True
+
+                    print(f"[line {line}] Error: Unterminated string.", file=sys.stderr)
+
+                else:
+
+                    print(f'STRING "{word}" {word}')
+
+            elif c.isalpha() or c == "_":
+
+                word = c
+
+                i += 1
+
+                while i < length and (
+
+                    file_contents[i].isalpha()
+
+                    or file_contents[i].isdigit()
+
+                    or file_contents[i] == "_"
+
+                ):
+
+                    word += file_contents[i]
+
+                    i += 1
+
+                #print(f"IDENTIFIER {word} null")
+
+                if word in reserved_words:
+
+                    print(f"{word.upper()} {word} null")
+
+                else:
+
+                    print(f"IDENTIFIER {word} null")
+
+                i -= 1
+
+            elif c.isdigit():
+
+                only0 = True
+
+                number = c
+
+                i += 1
+
+                while i < length and file_contents[i].isdigit():
+
+                    number += file_contents[i]
+
+                    i += 1
+
+                if i < length and file_contents[i] == ".":
+
+                    decimal_part = "."
+
+                    i += 1
+
+                    while i < length and file_contents[i].isdigit():
+
+                        if file_contents[i] != "0":
+
+                            only0 = False
+
+                        decimal_part += file_contents[i]
+
+                        i += 1
+
+                    if (
+
+                        len(decimal_part) > 1
+
+                    ):  # If there are digits after the decimal point
+
+                        number += decimal_part
+
+                    else:
+
+                        i -= 1
+
+                literal_value = number if "." in number else number + ".0"
+
+                if only0 and "." in number:
+
+                    literal_value = number[: number.index(".") + 2]
+
+                print(f"NUMBER {number} {literal_value}")
+
+                i -= 1  # Adjust for the main loop's increment
+
+            else:
+
+                error = True
+
+                print(
+
+                    f"[line {line}] Error: Unexpected character: {c}", file=sys.stderr
+
+                )
+
+            i += 1
+
+    print("EOF  null")
+
+    if error:
 
         exit(65)
+
+    exit(0)
 
 if __name__ == "__main__":
 
